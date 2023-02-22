a5d273b9f794946ebcd1c3f24ae75ac818b83e35
lionelsellam <lionelsellam@hotmail.com>
2023-02-22 21:09:11 +0200

    ✨ F01: LOGIN > Add title form

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  // State
+  const [inputValue, setInputValue] = useState("");
+
+  // Behavior
+  const handleSubmit = (event) => {
+    event.preventDefault();
+      alert(`Bonjour ${inputValue}`);
+      setInputValue("");
+    };
+
+    const handleChange = (event) => {
+      setInputValue(event.target.value);
+    };
+    
+
+  // Render
+  return (
+    <div>
+      <h1>Bienvenue chez-nous</h1>
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          value={inputValue}
+          onChange={handleChange}
+          type="text"
+          placeholder="Entrez votre prenom..."
+          required
+        />
+        <button>Accedez avotre espace</button>
+      </form>
+    </div>
+  );
 }
