53b3864ebaaafc1e34e14a814be7e5d054b0ebf4
lionelsellam <lionelsellam@hotmail.com>
2023-02-22 21:27:29 +0200

    ♻️ F01: LOGIN > Create LoginForm

---
+++ src/components/pages/login/LoginForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export default function LoginForm() {
+  // State
+  const [inputValue, setInputValue] = useState("");
+
+  // Behavior
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+    setInputValue("");
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  // Render
+  return (
+    <form action="submit" onSubmit={handleSubmit}>
+      <h1>Bienvenue chez-nous</h1>
+      <h2>Connectez-vous</h2>
+      <input
+        value={inputValue}
+        onChange={handleChange}
+        type="text"
+        placeholder="Entrez votre prenom..."
+        required
+      />
+      <button>Accedez avotre espace</button>
+    </form>
+  );
+}
