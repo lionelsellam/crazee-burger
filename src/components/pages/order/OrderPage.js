74e3b5082963cc6055d9c79ae73ca80c21f36baa
lionelsellam <lionelsellam@hotmail.com>
2023-02-27 18:00:48 +0200

    ✨ F02: LOGIN > add redirection from/to ErrorPage

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 export default function OrderPage() {
   return (
     <div>
-        <h1>orderPage</h1>
-        <Link to="/"><button>Deconnexion</button></Link>
+      <h1>OrderPage</h1>
+      <Link to="/">
+        <button>Deconnexion</button>
+      </Link>
     </div>
-  )
+  );
 }
