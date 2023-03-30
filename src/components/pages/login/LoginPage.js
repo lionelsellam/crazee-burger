4356ab6a5ae8e0db56d95234730c2294c1885474
lionelsellam <lionelsellam@hotmail.com>
2023-03-30 23:32:37 +0300

    💄 F03: LOGIN > Add styling and background

--- src/components/pages/login/LoginPage.js
+++ src/components/pages/login/LoginPage.js
@@ -4,20 +4,32 @@ import Logo from "../Reusable-ui/Logo";
 import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
-  // Render
   return (
     <LoginPageStyled>
-      <Logo/>
+      <Logo />
       <LoginForm />
     </LoginPageStyled>
   );
 }
 
 const LoginPageStyled = styled.div`
-  background: red;
   height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  ::before {
+    content: "";
+    background: url("/images/burgerFries-background.jpg");
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: darken;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
 `;
