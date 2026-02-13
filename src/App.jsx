e71d35ee8cde02862a484a2a294fd0de8be75577
Lionel Sellam <lionelsellam@hotmail.com>
2026-02-14 01:48:58 +0200

    ✨ F02: APP > installed routingsystem

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import "./App.css"
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 import LoginPage from "./components/pages/login/LoginPage";
+import OrderPage from "./components/pages/login/Order/OrderPage";
+import ErrorPage from "./components/pages/login/error/ErrorPage";
 
 function App() {
-  return <LoginPage/>
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/order" element={<OrderPage />} />
+      <Route path="*" element={<ErrorPage />} />
+
+    </Routes>
+  );
 }
 
 export default App;
