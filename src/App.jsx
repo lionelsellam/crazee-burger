8c5733967a6a44bb9c500805fcca1f65105f38ab
Lionel Sellam <lionelsellam@hotmail.com>
2026-02-14 02:56:42 +0200

    ✏️ F02: APP > fix OrderPage path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./components/pages/login/LoginPage";
-import OrderPage from "./components/pages/login/Order/OrderPage";
+import OrderPage from "./components/pages/login/order/OrderPage";
 import ErrorPage from "./components/pages/login/error/ErrorPage";
 
 function App() {
