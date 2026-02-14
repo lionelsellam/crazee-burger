9a888577ba9f1aa48f3342b2802c69750e9347c8
Lionel Sellam <lionelsellam@hotmail.com>
2026-02-15 01:29:55 +0200

    🔥 F03: LoginForm> reset LoginForm after style changes

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./LoginForm.css"
+import "./LoginForm.css";
 
 export default function LoginForm() {
   // State
@@ -19,37 +19,22 @@ export default function LoginForm() {
     setInputValue(event.target.value);
   };
 
-  const fruit = {
-    name: "abricot",
-    origin: "France",
-
-  }
-
-  const titleH2Style = {backgroundColor: "green", color:"white", fontSize: 20 }
-
   // Display (render)
 
   return (
     <form action="submit" onSubmit={handleSubmit}>
-      <h1 style={{color: "red" }}>Welcome to the Crazee Burger !</h1>
+      <h1>Welcome to the Crazee Burger !</h1>
       <br />
-      <h2 style={titleH2Style}>Login</h2>
+      <h2>Login</h2>
 
       <input
         value={inputValue}
         onChange={handleChange}
         type="text"
-        placeholder="Enter your name..."
+        placeholder="Enter your name"
         required
       />
-      <button>Accedez a votre espace</button>
+      <button>Accedez a mon espace</button>
     </form>
   );
 }
-
-/* methods for add style to composant 
-* 1. inline style 
-* 2. object style
-* 3. modules css ( avec class name )
-* 4. global style ( index.css )
-*/
