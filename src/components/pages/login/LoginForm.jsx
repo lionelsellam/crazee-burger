0600c35fc91c055e8853f3ac88d24824d1e35a32
Lionel Sellam <lionelsellam@hotmail.com>
2026-02-15 13:23:55 +0200

    💄F03: LOGIN > LoginForm > input > add styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import "./LoginForm.css";
 import "styled-components";
 import styled from "styled-components";
+import { BsPersonCircle } from "react-icons/bs";
 
 export default function LoginForm() {
   // State
@@ -29,20 +30,24 @@ export default function LoginForm() {
       <hr />
       <h2>Login</h2>
 
-      <input
-        value={inputValue}
-        onChange={handleChange}
-        type="text"
-        placeholder="Enter your name"
-        required
-      />
+      <div className="input-with-icon">
+        <BsPersonCircle className="icon" />
+
+        <input
+          value={inputValue}
+          onChange={handleChange}
+          type="text"
+          placeholder="Enter your name"
+          required
+        />
+      </div>
+
       <button>Accedez a mon espace</button>
     </LoginFormStyled>
   );
 }
 
 const LoginFormStyled = styled.form`
-  background: green;
   display: flex;
   flex-direction: column;
 
@@ -54,21 +59,48 @@ const LoginFormStyled = styled.form`
   border-radius: 5px;
   font-family: "Amatic SC", cursive;
 
-  hr{
+  hr {
     border: 1.5px solid #f56a2c;
     margin-bottom: 40px;
+    width: 400px;
   }
 
-  h1{
+  h1 {
     color: white;
     font-size: 48px;
   }
 
-  h2{
+  h2 {
     color: #8e8b8b;
     margin: 20px 10px 10px;
     color: white;
     font-size: 36px;
   }
 
+  .input-with-icon {
+    background-color: #fff;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    padding: 18px 24px;
+    margin: 18px 0;
+
+    .icon {
+      font-size: 15px;
+      margin-right: 8px;
+      color: #93a2b1;
+    }
+
+    input {
+      background: transparent;
+      border: none;
+      font-size: 15px;
+      color: #17161a;
+    }
+
+    &::placeholder {
+      background: white;
+      color: lightgrey;
+    }
+  }
 `;
