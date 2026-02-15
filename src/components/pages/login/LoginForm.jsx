f8df1fe18763962cc87f95c4af343a254e34a1c7
Lionel Sellam <lionelsellam@hotmail.com>
2026-02-15 14:02:28 +0200

    💄 F03: LOGIN> LoginForm > button > Add styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -4,6 +4,7 @@ import "./LoginForm.css";
 import "styled-components";
 import styled from "styled-components";
 import { BsPersonCircle } from "react-icons/bs";
+import { IoChevronForward } from "react-icons/io5";
 
 export default function LoginForm() {
   // State
@@ -42,7 +43,10 @@ export default function LoginForm() {
         />
       </div>
 
-      <button>Accedez a mon espace</button>
+      <button className="button-with-icon">
+        <span>Accedez a mon espace</span>
+        <IoChevronForward className="icon"/>
+      </button>
     </LoginFormStyled>
   );
 }
@@ -101,6 +105,55 @@ const LoginFormStyled = styled.form`
     &::placeholder {
       background: white;
       color: lightgrey;
+    }
+  }
+
+  .button-with-icon {
+    width: 100%;
+    border: 1px solid red;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    white-space: nowrap;
+    text-decoration: none;
+    line-height: 1;
+
+    padding: 18px 24px;
+    border-radius: 5px;
+    font-size: 15px;
+    font-weight: 800;
+    color: white;
+    background-color: #ff9f1b;
+    border: 1px solid #ff9f1b;
+
+    &:hover:not(:disabled) {
+      background-color: white;
+      color: #ff9f1b;
+      transition: all 200ms ease-out;
+    }
+
+    &:active {
+      color: white;
+      background-color: #ff9f1b;
+      border: 1px solid #ff9f1b;
+    }
+
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+
+    .icon {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 15px;
+      margin-left: 10px;
+    }
+
+    span{
+      font-size: 25px;
     }
   }
 `;
