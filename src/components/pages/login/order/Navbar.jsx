9cd8d4c615a463751329c1b0cbd7609a5c6a7375
Lionel Sellam <lionelsellam@hotmail.com>
2026-02-23 22:51:28 +0200

    ✨ F04: ORDER > Navbar > trigger reload on click

--- src/components/pages/login/order/Navbar.jsx
+++ src/components/pages/login/order/Navbar.jsx
@@ -1,18 +1,21 @@
 import styled from "styled-components";
 import NavbarRightSide from "./NavbarRightSide";
-import Logo from "../../../reusable-ui/Logo"
+import Logo from "../../../reusable-ui/Logo";
 import theme from "../../../../theme";
+import { refreshPage } from "../../../../utils/window";
+
+
 export default function Navbar({ username }) {
   return (
     <NavbarStyled>
-      <Logo/>
+      <Logo className="logo-order-page" onClick={() => refreshPage()} />
       <NavbarRightSide username={username} />
     </NavbarStyled>
   );
 }
 
 const NavbarStyled = styled.nav`
-background: ${theme.colors.white};
+  background: ${theme.colors.white};
   height: 10vh;
   display: flex;
   justify-content: space-between;
@@ -20,4 +23,8 @@ background: ${theme.colors.white};
 
   border-top-left-radius: ${theme.borderRadius.extraRound};
   border-top-right-radius: ${theme.borderRadius.extraRound};
+
+  .logo-order-page{
+    cursor: pointer;
+  }
 `;
---
+++ src/utils/window.jsx
@@ -0,0 +1,2 @@
+
+export const refreshPage = () => window.location.reload();
