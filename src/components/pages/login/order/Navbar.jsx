8ddf601ca26ef1926be47abf2c2b92bdbc983b78
Lionel Sellam <lionelsellam@hotmail.com>
2026-02-23 14:06:35 +0200

    🚧   F04: ORDER > layout > navbar >  add left-side and right-side

--- src/components/pages/login/order/Navbar.jsx
+++ src/components/pages/login/order/Navbar.jsx
@@ -4,11 +4,14 @@ import styled from "styled-components";
 export default function Navbar({ username }) {
   return (
     <NavbarStyled>
-      NavBar
-      <h1>Bonjour {username}</h1>
-      <Link to="/">
-        <button>Logged out</button>
-      </Link>
+      <div className="left-side">Left</div>
+      <div className="right-side">
+        Right
+        <h1>Bonjour {username}</h1>
+        <Link to="/">
+          <button>Logged out</button>
+        </Link>
+      </div>
     </NavbarStyled>
   );
 }
@@ -16,4 +19,14 @@ export default function Navbar({ username }) {
 const NavbarStyled = styled.nav`
   background: blue;
   height: 10vh;
+  display: flex;
+  justify-content: space-between;
+
+  .left-side {
+    background: pink;
+  }
+
+  .right-side {
+    background: purple;
+  }
 `;
