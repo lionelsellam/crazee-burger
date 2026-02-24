f9c3645083ad65f4c7c5c2427f846a5e2a4bac56
Lionel Sellam <lionelsellam@hotmail.com>
2026-02-24 22:31:23 +0200

    💄 F05: ORDER > MENU > Add final style

--- src/components/pages/login/order/Main/Menu.jsx
+++ src/components/pages/login/order/Main/Menu.jsx
@@ -1,24 +1,30 @@
 import { useState } from "react";
 import styled from "styled-components";
-import {fakeMenu2 } from "../../../../../fakeData/fakeMenu";
+import theme from "../../../../../theme";
+import { fakeMenu2 } from "../../../../../fakeData/fakeMenu";
 
 export default function Menu() {
   const [menu, setMenu] = useState(fakeMenu2);
 
-  return <MenuStyled className="menu">{menu.map((produit)=> {
-    return <div className="produit">{produit.title}</div>
-  })}</MenuStyled>;
+  return (
+    <MenuStyled className="menu">
+      {menu.map((produit) => {
+        return <div className="produit">{produit.title}</div>;
+      })}
+    </MenuStyled>
+  );
 }
 
 const MenuStyled = styled.div`
-  background: purple;
+  background: ${theme.colors.background_white};
   display: grid;
-  grid-template-columns: repeat(4,1fr);
+  grid-template-columns: repeat(4, 1fr);
   grid-row-gap: 60px;
   padding: 50px 50px 150px;
   justify-items: center;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
-  .produit{
+  .produit {
     background: red;
     width: 240px;
     height: 330px;
